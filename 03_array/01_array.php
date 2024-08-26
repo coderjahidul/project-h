f233c4c2d8f9224a5ff19b7820551af7791fb17d
md jahidul islam sabuz <sobuz0349@gmail.com>
2024-08-27 01:37:21 +0600

    add 01_array.php file and explain array data type function in php

---
+++ 03_array/01_array.php
@@ -0,0 +1,101 @@
+<?php
+// PHP array
+// $students = array(
+//     "Jahidul",
+//     "Islam",
+//     "Rahman",
+//     "Rahim",
+//     125,
+//     true,
+//     array(1, 2, 3),
+//     null
+// );
+
+//echo $students[2]; // Returns value is Rahman
+// echo PHP_EOL;
+// echo $students[1]; // Returns value is Islam
+
+// print_r($students); // Prints Array ( [0] => Jahidul, [1] => Islam, [2] => Rahman, [3] => Rahim, [4] => 125, [5] => true, [6] => Array ( [0] => 1, [1] => 2, [2] => 3 ), [7] => null )
+
+// echo PHP_EOL; // Prints new line
+
+// var_dump($students); // Prints Array ( [0] => string(6) "Jahidul" [1] => string(5) "Islam" [2] => string(6) "Rahman" [3] => string(5) "Rahim" [4] => int(125) [5] => bool(true) [6] => array(3) [7] => NULL )
+
+// print($students[0]); // Prints Jahidul
+
+// echo count($students); // Count the number of elements in the array value is 8
+
+// echo $students[6][1]; // Prints array ( [0] => 1, [1] => 2, [2] => 3 )
+
+//$students = array("Jahidul", "Islam", "Rahman", "Rahim"); // Declare an array
+//$students = ["Jahidul", "Islam", "Rahman", "Rahim"]; // Declare an array
+
+// loop for through the array 
+// for($i = 0; $i < count($students); $i++){
+//     echo $students[$i] . "\n";
+// }
+
+// reverse loop for through the array
+// $n = count($students);
+// for($i = $n - 1; $i >= 0; $i--){
+//     echo $students[$i] . "\n";
+// }
+
+// foreach loop for through the array
+// foreach($students as $student){
+//     echo $student . "\n";
+// }
+
+// while loop for through the array
+// $i = 0;
+// while($i < count($students)){
+//     echo $students[$i] . "\n";
+//     $i++;
+// }
+
+// do while loop for through the array
+// $i = 0; 
+// do {
+//     echo $students[$i] . "\n";
+//     $i++;
+// } while ($i < count($students));
+
+// break and continue
+// for($i = 0; $i < count($students); $i++){
+//     if($students[$i] == "Rahman"){
+//         break;
+//     }
+//     echo $students[$i] . "\n";
+// }
+
+// for($i = 0; $i <count($students); $i++){
+//     if($students[$i] == "Rahman"){
+//         continue;
+//     }
+
+//     echo $students[$i] . "\n";
+// }
+
+// goto statement
+// $i = 0;
+// start:
+// echo $students[$i] . "\n";
+// $i++;
+// if($i < count($students)){
+//     goto start;
+// }
+
+// echo "Starting of the program\n";
+
+// goto start; // Jumps to the label start
+
+// echo "I Love my country\n";
+// echo "Amar sonar Bangla ame tomai valobase\n";
+// echo "I am Jahidul\n";
+// echo "I am Programmer\n";
+// echo "I love my country\n";
+
+// start:
+// echo "End of the program\n"; 
+
+
