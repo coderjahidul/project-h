ab6818b545d2ca7a54c83e8d5f266958c02e7bed
coderjahidul <sobuz0349@gmail.com>
2024-11-05 14:49:10 +0600

    explain nl2br function

---
+++ 06_string/08_nl2br.php
@@ -0,0 +1,16 @@
+<?php 
+    $string = "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n Rerum animi cupiditate quos! Numquam voluptatibus ipsam laborum quae dolorem ducimus quia, asperiores magnam debitis, vel labore rerum. Id provident deserunt totam dolorem ex tempore facere, quis, quam in dicta similique officia vel, nisi vero mollitia omnis quidem ab cum sit facilis? Veritatis recusandae et ipsam? Cumque sapiente ipsam vero quam nostrum dicta aliquid incidunt magni quibusdam magnam fugit doloribus quo unde qui at dolorem minima, in amet alias! Exercitationem voluptatum adipisci autem quia ducimus nulla totam magnam rem, explicabo inventore ad voluptates fugiat recusandae et commodi veritatis dolore aut fugit beatae, in, sed quos illo suscipit sint.\n\n Ipsa accusantium, reprehenderit cupiditate recusandae suscipit voluptatum, fugiat sed et veniam obcaecati vitae qui nobis nemo aspernatur vero incidunt labore maiores fugit consequatur? Doloribus accusantium consequatur expedita necessitatibus animi optio omnis nobis! Autem aperiam deserunt voluptatibus quia minima consequatur obcaecati minus corrupti molestiae placeat delectus voluptate ab a est, nam deleniti voluptatum pariatur! Nemo, vero nobis? Saepe repellat perspiciatis optio rerum minima voluptatibus ratione nisi fuga.\n Ratione illum est dolorum reiciendis non numquam eum iste, dicta praesentium vel, aperiam quidem aliquid assumenda error.\n Consequuntur similique, fuga doloribus sunt unde tenetur nihil aspernatur sint laudantium quam saepe. Neque aperiam labore architecto fugit.\n \n Dolore deserunt, voluptatibus sapiente non dolorum porro ab delectus molestiae dolores temporibus voluptatum.";
+
+    echo $string;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo nl2br($string);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    $string = "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n\n Rerum animi cupiditate quos! Numquam voluptatibus ipsam laborum quae dolorem ducimus quia, asperiores magnam debitis, vel labore rerum.\n Id provident deserunt totam dolorem ex tempore facere, quis, quam in dicta similique officia vel, nisi vero mollitia omnis quidem ab cum sit facilis? Veritatis recusandae et ipsam? Cumque sapiente ipsam vero quam nostrum dicta aliquid incidunt magni quibusdam magnam fugit doloribus quo unde qui at dolorem minima, in amet alias! Exercitationem voluptatum adipisci autem quia ducimus nulla totam magnam rem, explicabo inventore ad voluptates fugiat recusandae et commodi veritatis dolore aut fugit beatae, in, sed quos illo suscipit sint.\n\n";
+
+    echo nl2br($string);
