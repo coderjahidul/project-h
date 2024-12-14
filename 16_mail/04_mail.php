a15d4d16a027dc2693112332201edfdbded8082a
coderjahidul <sobuz0349@gmail.com>
2024-12-14 18:57:43 +0600

    explain phpmailer function with smtp server

--- 16_mail/04_mail.php
+++ 16_mail/04_mail.php
@@ -1,15 +1,25 @@
 <?php
-// PHPMailer function
+// PHPMailer function with SMTP
 require_once('./16_mail/PHPMailer-master/PHPMailer.php');
 require_once('./16_mail/PHPMailer-master/Exception.php');
+require_once('./16_mail/PHPMailer-master/SMTP.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $pm = new PHPMailer(true);
 
-try {
-    $pm->setFrom("test@test.com");
+try{
+    $pm->SMTPDebug = 2;
+    $pm->isSMTP(true);
+    $pm->Host = 'mail.hasin.me';
+    $pm->SMTPAuth = true;
+    $pm->Username = "test@hasin.me";
+    $pm->Password = "--!F&7RZ@Kuh";
+    $pm->SMTPSecure = 'tls';
+    $pm->Port = 587;
+    
+    $pm->setFrom("test@hasin.me");
     $pm->addAddress("sobuz0349@gmail.com");
     $pm->Subject = "Here is the invoice";
     $pm->Body = "Hi, Hero is the <strong>Invoice</strong> from your last purchase";
@@ -18,6 +28,7 @@ try {
     $pm->addAttachment("./google-information.pdf");
     $pm->send();
     echo "Email has been sent";
-}catch (Exception $e){
-    echo "Failed: " . $e->getMessage();
+}catch(Exception $e){
+    echo "Message could not be sent. Mailer Error: {$e->getMessage()}";
 }
+
