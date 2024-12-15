90b0d3a99595103b2ee773a2a1f6aceaa912072c
coderjahidul <sobuz0349@gmail.com>
2024-12-15 18:02:40 +0600

    explain php error funciotn

---
+++ 17_php_error/01_php_error.php
@@ -0,0 +1,11 @@
+<?php
+// error_reporting(0); // turn off error reporting
+// error_reporting(E_ALL); // turn on all error reporting
+// error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); // turn on error reporting level = E_ERROR | E_WARNING | E_PARSE | E_NOTICE
+
+// echo get_cfg_var("cfg_file_path"); // get configuration file path php.ini
+
+// ini_set('display_errors', 0); // turn off error reporting
+// ini_set('display_startup_errors', 0); // turn off error reporting
+
+echo $book;
