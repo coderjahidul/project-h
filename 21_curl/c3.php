f92a9bab96d2813cf7653f9481b572c464764315
coderjahidul <sobuz0349@gmail.com>
2024-12-28 18:41:29 +0600

    explain file download with curl

---
+++ 21_curl/c3.php
@@ -0,0 +1,7 @@
+<?php 
+set_time_limit(0); // if file is large
+$ch = curl_init("https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg");
+$file_name = fopen("image.jpg", "w");
+curl_setopt($ch, CURLOPT_FILE, $file_name);
+curl_exec($ch);
+?>
