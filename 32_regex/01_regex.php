bd13c82278de45c0733e79d9b1edff5fe2808257
coderjahidul <sobuz0349@gmail.com>
2025-01-15 16:24:43 +0600

    create regex file

---
+++ 32_regex/01_regex.php
@@ -0,0 +1,11 @@
+<?php 
+    $str = <<<STRING
+    quick brown
+    fox jumps over the
+    lazy dog
+    STRING;
+    // print_r(explode(" ", $str));
+    // echo count(explode(" ", $str));
+ 
+
+?>
